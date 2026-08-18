e1aca36e4a0516fa21ce3908366c8c99b3bddb5c
Thompson Bear <thompsonbear21@gmail.com>
2026-08-18 12:20:06 -0400

    fix(workload): update kubectl_manifest from manifest to yaml_body

--- workload/modules/core/istio/main.tf
+++ workload/modules/core/istio/main.tf
@@ -2,7 +2,7 @@ locals {
   istio_repo = "https://istio-release.storage.googleapis.com/charts"
   gateway_api_crds = {
     for manifest in provider::kubernetes::manifest_decode_multi(file("${path.module}/resources/gateway-api-crds.yaml")) :
-      "${manifest.kind}/${manifest.metadata.name}" => { for key, value in manifest : key => value if key != "status" }
+    "${manifest.kind}/${manifest.metadata.name}" => { for key, value in manifest : key => value if key != "status" }
   }
   gateways = {
     public  = { ip = cidrhost(var.ip_pool, 0) }
@@ -66,7 +66,7 @@ resource "helm_release" "ztunnel" {
 resource "kubectl_manifest" "gateways" {
   depends_on = [helm_release.ztunnel]
   for_each   = local.gateways
-  manifest = yamlencode({
+  yaml_body = yamlencode({
     apiVersion = "gateway.networking.k8s.io/v1"
     kind       = "Gateway"
     metadata = {
