ba8f75b1001be0259f282675cc32350b7f4e76fb
Thompson Bear <thompsonbear21@gmail.com>
2026-08-24 11:15:20 -0400

    fix(workload): adjust naming on letsencrypt clusterissuer

--- workload/modules/core/cert-manager/main.tf
+++ workload/modules/core/cert-manager/main.tf
@@ -31,13 +31,13 @@ resource "kubectl_manifest" "public_acme_issuer" {
     "apiVersion" = "cert-manager.io/v1"
     "kind"       = "ClusterIssuer"
     "metadata" = {
-      "name" = "public-acme"
+      "name" = "letsencrypt"
     }
     "spec" = {
       "acme" = {
         "email" = var.acme_email
         "privateKeySecretRef" = {
-          "name" = "public-acme"
+          "name" = "letsencrypt"
         }
         "server" = var.environment == "prod" ? "https://acme-v02.api.letsencrypt.org/directory" : "https://acme-staging-v02.api.letsencrypt.org/directory"
         "profile" = "tlsserver"
