12344a0dd68eab5066dadfe01577852eb581ed2f
Thompson Bear <thompsonbear21@gmail.com>
2026-09-08 21:44:20 -0400

    re-enable keycloak

--- workload/main.tf
+++ workload/main.tf
@@ -61,7 +61,7 @@ module "cnpg_operator" {
 
 module "valkey_operator" {
   source = "./modules/system/valkey-operator"
-  tag    = var.system.valkey_tag
+  tag = var.system.valkey_tag
 }
 
 module "app_namespaces" {
@@ -78,49 +78,49 @@ locals {
   }
 }
 
-# module "keycloak_app" {
-#   depends_on = [module.cnpg_operator, module.mayastor, module.cert_manager, module.app_namespaces]
-#   source     = "./modules/app"
-#   context    = local.app_context
-#   app_name   = "keycloak"
-#   namespace  = "keycloak"
-#   replicas   = 1
-#   image_tag  = var.system.keycloak_tag
-#   backend    = { service = "keycloak", port = 8080 }
-#   dns        = { labels = ["auth"], public = true }
-#   secrets    = { admin-secret = module.akv.secrets.admin-secret }
-#   postgres   = { base_gb = 20, wal_gb = 10, replicas = 1 }
-# }
+module "keycloak_app" {
+  depends_on = [module.cnpg_operator, module.mayastor, module.cert_manager, module.app_namespaces]
+  source     = "./modules/app"
+  context    = local.app_context
+  app_name   = "keycloak"
+  namespace  = "keycloak"
+  replicas   = 1
+  image_tag  = var.system.keycloak_tag
+  backend    = { service = "keycloak", port = 8080 }
+  dns        = { labels = ["auth"], public = true }
+  secrets    = { admin-secret = module.akv.secrets.admin-secret }
+  postgres   = { base_gb = 20, wal_gb = 10, replicas = 1 }
+}
 
-# resource "keycloak_realm" "this" {
-#   depends_on   = [module.keycloak_app, module.valkey_operator]
-#   realm        = "${module.akv.secrets.keycloak-realm-prefix}-${var.environment}"
-#   display_name = var.environment == "prod" ? title(module.akv.secrets.keycloak-realm-prefix) : "${title(module.akv.secrets.keycloak-realm-prefix)} ${capitalize(var.environment)}"
-# }
+resource "keycloak_realm" "this" {
+  depends_on   = [module.keycloak_app, module.valkey_operator]
+  realm        = "${module.akv.secrets.keycloak-realm-prefix}-${var.environment}"
+  display_name = var.environment == "prod" ? title(module.akv.secrets.keycloak-realm-prefix) : "${title(module.akv.secrets.keycloak-realm-prefix)} ${capitalize(var.environment)}"
+}
 
-# resource "keycloak_oidc_identity_provider" "microsoft_entra_idp" {
-#   realm        = keycloak_realm.this.id
-#   alias        = "entra" # https://<KEYCLOAK_HOSTNAME>/realms/<REALM>/broker/entra/endpoint
-#   display_name = "Microsoft Entra"
+resource "keycloak_oidc_identity_provider" "microsoft_entra_idp" {
+  realm        = keycloak_realm.this.id
+  alias        = "entra" # https://<KEYCLOAK_HOSTNAME>/realms/<REALM>/broker/entra/endpoint
+  display_name = "Microsoft Entra"
 
-#   client_id     = module.akv.secrets.entra-app.client_id
-#   client_secret = module.akv.secrets.entra-app.client_secret
+  client_id     = module.akv.secrets.entra-app.client_id
+  client_secret = module.akv.secrets.entra-app.client_secret
 
-#   issuer            = "https://login.microsoftonline.com/${module.akv.secrets.entra-app.tenant_id}/v2.0"
-#   authorization_url = "https://login.microsoftonline.com/${module.akv.secrets.entra-app.tenant_id}/oauth2/v2.0/authorize"
-#   token_url         = "https://login.microsoftonline.com/${module.akv.secrets.entra-app.tenant_id}/oauth2/v2.0/token"
-#   logout_url        = "https://login.microsoftonline.com/${module.akv.secrets.entra-app.tenant_id}/oauth2/v2.0/logout"
-#   jwks_url          = "https://login.microsoftonline.com/${module.akv.secrets.entra-app.tenant_id}/discovery/v2.0/keys"
-#   user_info_url     = "https://graph.microsoft.com/oidc/userinfo"
-#   default_scopes    = "openid offline_access"
+  issuer            = "https://login.microsoftonline.com/${module.akv.secrets.entra-app.tenant_id}/v2.0"
+  authorization_url = "https://login.microsoftonline.com/${module.akv.secrets.entra-app.tenant_id}/oauth2/v2.0/authorize"
+  token_url         = "https://login.microsoftonline.com/${module.akv.secrets.entra-app.tenant_id}/oauth2/v2.0/token"
+  logout_url        = "https://login.microsoftonline.com/${module.akv.secrets.entra-app.tenant_id}/oauth2/v2.0/logout"
+  jwks_url          = "https://login.microsoftonline.com/${module.akv.secrets.entra-app.tenant_id}/discovery/v2.0/keys"
+  user_info_url     = "https://graph.microsoft.com/oidc/userinfo"
+  default_scopes    = "openid offline_access"
 
-#   sync_mode          = "IMPORT"
-#   trust_email        = true
-#   validate_signature = true
-# }
+  sync_mode          = "IMPORT"
+  trust_email        = true
+  validate_signature = true
+}
 
 module "apps" {
-  # depends_on = [module.keycloak_app]
+  depends_on = [module.keycloak_app]
   for_each   = var.apps
   source     = "./modules/app"
   context    = local.app_context
@@ -149,7 +149,7 @@ module "apps" {
   }
 
   secrets  = try(each.value.secrets, {})
-  # keycloak = try(each.value.keycloak, null)
+  keycloak = try(each.value.keycloak, null)
   postgres = try(each.value.postgres, null)
   valkey   = try(each.value.valkey, null)
 }
