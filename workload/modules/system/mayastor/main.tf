c723667288d32b8739a462beed9437ac368fd743
Thompson Bear <thompsonbear21@gmail.com>
2026-09-10 19:37:26 -0400

    fix(workload): set nfs replicas to 1

--- workload/modules/system/mayastor/main.tf
+++ workload/modules/system/mayastor/main.tf
@@ -76,7 +76,7 @@ resource "kubernetes_deployment_v1" "nfs_server_deploy" {
     namespace = module.namespace.name
   }
   spec {
-    replicas = 2
+    replicas = 1
     selector {
       match_labels = {
         role = "nfs-server"
